f6301e290d07051cd4c687eb526afde9d67bd936
Isaac Oduh <ufuomaoduh@gmail.com>
2018-05-10 05:46:30 +0100

    Heroku Logfile

--- config/app.php
+++ config/app.php
@@ -120,7 +120,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    // 'log' => env('APP_LOG', 'single'),
+    'log' =>'error_log',
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
